6a85175def74cbacee3d11311c18bb57c2e68294
zachary <zmh012004@gmail.com>
2023-10-03 01:32:54 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-// Write your code here!
 const element = document.createElement("div");
-document.body.append(element);
-const Ul = document.createElement("ul");
+document.body.appendChild(element);
+
+const ul = document.createElement("ul");
+let liHTML = "";
 for (let i = 0; i < 3; i++) {
-  const li = document.createElement("li");
-  li.textContent = (i + 1).toString();
-  ul.append(li);
+  liHTML += `<li>${i + 1}</li>`;
 }
-element.append(ul);
-li.textContent = "Hi there!";
-// => <li>Hi there!</li>
-console.log(li.textContent);
-// => "Hi there!"
-<main id="main">
+ul.innerHTML = liHTML;
+element.appendChild(ul);
+
+const newLi = document.createElement("li");
+newLi.textContent = "Hi there!";
+ul.replaceChild(newLi, ul.children[0]);
+
+const main = document.createElement("main");
+main.id = "main";
+main.classList.add("pet-listing", "dog");
+main.innerHTML = `
   <h1>Poodles!</h1>
-  <h3>
-    An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a
-    post-Frankfurt School Appropriationist Perspective
-  </h3>
+  <h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3>
   <p><em>By: Byron Q. Poodle, Esq., BA.</em></p>
-</main>
-constelement = document.getElementById("main");
+`;
+document.body.appendChild(main);
+
 element.style.height = "300px";
-element.style.backgroundColor = "#27647B"
+element.style.backgroundColor = "#27647B";
 element.textContent = "You've changed what's on the screen!";
 element.style.fontSize = "24px";
 element.style.marginLeft = "30px";
-element.style.lineHeight = 2;
-element.className = "pet-listing dog";
-<main id="main" class="pet-listing dog"></main>
+element.style.lineHeight = "2";
 element.classList.remove("dog");
 element.classList.add("cat", "sale");
-<main id="main" class="pet-listing cat sale"></main>
-const ul = document.getElementsByTagName("ul")[0];
-const secondChild = ul.querySelector("li:nth-child(2)");
-ul.removeChild(secondChild);
+
+ul.removeChild(ul.children[1]);
+
+
+const mainElement = document.getElementById("main");
+if (mainElement) {
+  mainElement.remove(main);
+}
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Zach Haddad is the champion"; // Replace "YOUR-NAME" with your desired name
+
+console.log(newHeader); // Check if the newHeader is created correctly
